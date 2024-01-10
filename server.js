c6c870cfdc0cd6370a26772c199f0e5373599778
RawabyRahal <rawaby.16.4@gmail.com>
2024-01-10 17:12:16 +0200

    updating weather app

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const weatherAPI = require('./routes/api')
+const weatherAPI = require('./server/routes/api')
 const mongoose = require('mongoose')
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/weatherDB", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/weatherDB", {
   useNewUrlParser: true,
 }).catch((err)=> console.log(err))
 
 
-app.use(express.static(path.join(__dirname, '../dist')))
+app.use(express.static(path.join(__dirname, './dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 
 
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use('/', weatherAPI)
 
 
-const port = 8585
-app.listen(port, function () {
-    console.log(`Running on port ${port}`)
+const PORT = 8585
+app.listen(process.env.PORT || PORT, function () {
+    console.log(`Running on port ${PORT}`)
 })
